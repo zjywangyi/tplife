4155d0530092d62c89b658766fe3b2446d102a2d
zhoujy <zhoujy0411@tpl.cntaiping.com>
2016-11-16 17:55:28 +0800

    Merge branch 'master' into dev

--- app/src/main/java/tplife/com/tplife/MainActivity.java
+++ app/src/main/java/tplife/com/tplife/MainActivity.java
@@ -1,13 +1,31 @@
 package tplife.com.tplife;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        super.onCreate(savedInstanceState);
+    }
+
+
+    private void abv() {
+
+
+    }
+
+    private void ABD(int a) {
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+        }
     }
 }
