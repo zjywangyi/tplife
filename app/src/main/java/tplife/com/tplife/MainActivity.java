c3a8aa988c5b9de6c9e1675e0d48f9ec415dc903
zhoujy <zjywangyi@163.com>
2017-02-21 14:38:10 +0800

    mod test

--- app/src/main/java/tplife/com/tplife/MainActivity.java
+++ app/src/main/java/tplife/com/tplife/MainActivity.java
@@ -1,29 +1,75 @@
 package tplife.com.tplife;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.support.design.widget.TabLayout;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+import tplife.com.tplife.common.PlayerManager;
+
+public class MainActivity extends AppCompatActivity implements PlayerManager.PlayerStateListener{
+
+    private PlayerManager player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_main);
-
         super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main);
+//        android.support.design.widget.TabLayout tabLayout=null;
+//
+//
+//        tabLayout.addTab(tabLayout.newTab().setText("Tab 1").setIcon(R.mipmap.ic_launcher));
+
+        double ipad2 = (double) (20 * (132 / 160) * (9.7 / (Math.sqrt(1024 * 1024 + 768 * 768))));
+        System.out.println(ipad2+"---");
+//        if (Build.VERSION.SDK_INT >= 21) {
+//            View decorView = getWindow().getDecorView();
+//            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+//            decorView.setSystemUiVisibility(option);
+//            getWindow().setStatusBarColor(Color.TRANSPARENT);
+//        }
+//        //透明状态栏
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+////        //透明导航栏
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+
+
+        change(this.str, this.anInt,this.ch);
+
+        System.out.print("****"+this.str + " and ");
+        System.out.print(this.anInt);
+        System.out.print(this.ch);
+
+        //initPlayer();
+
+        System.out.print("DDDDDD");
     }
 
 
 
-    private void ABDds(int a) {
-        try {
+    String str = new String("good");
+    int anInt=3;
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-        }
+    char[] ch = { 'a', 'b', 'c' };
 
+
+
+    public void change(String str, int anInt,char chd[]) {
+
+        str.toUpperCase();
+        anInt=5;
+        chd[0]='f';
 
     }
 
@@ -57,4 +103,38 @@ public class MainActivity extends AppCompatActivity {
         } finally {
         }
     }
+
+
+    private void initPlayer() {
+        player = new PlayerManager(this);
+        player.setFullScreenOnly(true);
+        player.setScaleType(PlayerManager.SCALETYPE_FILLPARENT);
+        player.playInFullScreen(true);
+
+        player.setPlayerStateListener(this);
+
+        player.play("http://zv.3gv.ifeng.com/live/zhongwen800k.m3u8");
+
+    }
+
+    @Override
+    public void onComplete() {
+
+    }
+
+    @Override
+    public void onError() {
+
+    }
+
+    @Override
+    public void onLoading() {
+
+    }
+
+    @Override
+    public void onPlay() {
+
+    }
+
 }
